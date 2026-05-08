802653c8b0148e6f131e2c158551274f7abdf58f
Isabella Alves <docarmoisabella895@gmail.com>
2026-05-08 09:17:59 -0300

    Atualização do Projeto

--- python/Aula 10/decimo.py
+++ python/Aula 10/decimo.py
@@ -17,53 +17,58 @@
 # O sistema deve manter um contador de vagas ocupadas. Se o estacionamento estiver lotado, a
 # cancela de entrada não deve abrir para novos clientes (exceto para quem possui Tag de Acesso
 # Rápido, pois estes têm vagas reservadas).
+while True:
+    import time
+    vaga = 500
+    tag_vaga = 50
+    print("Seja bem vindo ao shopping center")
+    print("Selecione sua vaga")
+    print("Opção 1 = Entrada via Ticket")
+    print("Opção 2 = Entrada via Tag")
+    opção = (input("Opção:"))
+    time.sleep(1.0)
+    if opção == "1":
+            print("Você escolheu a opção vaga via tictek.")
+            vaga__via_tictek = -1
+            time.sleep(1.5)
+            print("Verificando se há vagas diponiveis...")
+            time.sleep(1.5)
+            entrada = float(input("Digite a hora da entrada:"))
+            saida = float(input("Digite o horario de saída:"))
+            print("Calculando o tempo de saída...")
+            time.sleep(1.5)
+            print("Imprimindo seu ticket ")
+            time.sleep(2.0)
+            print("Tenha um bom passeio")
+            tempo = (saida - entrada)
+            resposta = input("Você pagou? (sim/não): ")
+            if resposta == "não":
+                print("Pagamento não realizado volte e faça seu pagamento!\nSe houve perda do ticket será cobrado uma taxa de R$50.00")
+            if resposta == "sim":
+                print("Pagamento confirmado!")
+                time.sleep(1.5)
+                if tempo <= 0.15:
+                    valor = 0
+                    time.sleep(1.5)
+                    print("Você ficou menos que 15 min, não há custo")
+                elif tempo <= 3:
+                    valor = 15
+                    time.sleep(2.0)
+                    print("Você pagou 15,00")
+                elif tempo >3:
+                    valor_sem_extra = 15
+                    valor_adicional = 3
+                    pagamento =(tempo - 3)*valor_adicional
+                    valor_total = pagamento + valor_sem_extra
+                    time.sleep(1.5)
+                    print("Você pagará:",valor_total,)
+                time.sleep
+                print("Volte sempre!")
+    elif opção == "2":
+        print("Você escolheu vaga via tag.")
+        time.sleep(0.5)
+        tag_vaga = -1
+        print("O valor será descontado em seu cartão \n Liberando cancela...")
 
-vaga_sem_reserva = 500
-print("seja bem vindo ao shopping center")
-print("selecione sua vaga")
-print("opção 1 = venho sem reserva")
-print("opção 2 = vaga_sem_reserva")
-
-opção = (input("Opção:"))
-if opção == "1":
-    print("Você escolheu vaga sem reserva.")
-    vaga_sem_reserva = -1
-    print("Verificando se a vagas diponiveis...")
-    entrada = int(input("dígite a hora da estrada:"))
-    saida = int(input("Dígite o horario de saída:"))
-    print("Calculando o tempo de saída...")
-print("imprimindo seu ticket ")
-print("tenha um bom passeio")
 
 
-tempo = (saida - entrada)
-if tempo <= 0.15:
-    valor = 0
-    print("voçê ficou somente 15 min não a custo")
-elif tempo <= 3:
-    valor = 15
-    print("você pagará 15,00")
-
-else:
-    valor_mais_Extra = 15
-    valor_adicional = 3
-   
-    pagamento =(tempo + 3)
-valor_total = (valor_mais_Extra * valor_adicional)
-print("você pagará:",valor_total,)
-print("Volte sempre")
-resposta = input("Você pagou? (sim/não): ")
-if resposta == "sim":
-    print("Pagamento confirmado!")
-elif resposta == "nao":
-    print("pagamento realizado não liberado!")
-
-elif:
-opção == "2"
-
-print("Você escolheu vaga com reserva.")
-print("Sua vaga esta reservada")
-entrada_vip = int(input("dígite o horario de entrada!:"))
-saída_vip = int(input("Dígite o horario de saída:"))
-print("muito obrigada o valor a ser pago sera descontado no seu ticket")
-ticket = 0 
